12f925e809d5e3211ef12fa7ec6d4a23f7ef040d
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-03-14 21:35:16 -0400

    Add the full net/http client interface methods

--- httpclient.go
+++ httpclient.go
@@ -1,10 +1,16 @@
 package interfaces
 
 import (
+	"io"
 	"net/http"
+	"net/url"
 )
 
 // Interface maps to http.Client
 type HTTPClient interface {
 	Do(*http.Request) (*http.Response, error)
+	Get(string) (*http.Response, error)
+	Head(string) (*http.Response, error)
+	Post(string, string, io.Reader) (*http.Response, error)
+	PostForm(string, url.Values) (*http.Response, error)
 }
